e5eb32cb094431d83e30a53c88aba364e78aeb6e
Ahmed <75604506+AhmedSaadounn@users.noreply.github.com>
2022-06-25 03:52:28 +0300

    Add files via upload

---
+++ src/comparison.java
@@ -0,0 +1,15 @@
+
+import java.util.Arrays;
+public class comparison{
+  public static void main(String[]args){
+// comparison Arrays
+    int[] array1 = new int [] {21,32,36,47};
+        int[] array2 = new int [] {11,22,33,44};
+    int[] array3 = new int [] {21,32,36,47};
+
+    System.out.println("Are array1 and array2 equal? ["+Arrays.equals(array1, array2)+"]");
+        System.out.println("Are array1 and array3 equal? ["+Arrays.equals(array1, array3)+"]");
+
+    
+  }
+}
